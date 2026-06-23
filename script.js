07fcbc7cf58d9729f96eb6554f5955ae9a07fc27
barilyahya <barilyahya288@gmail.com>
2026-06-23 15:06:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -589,8 +589,8 @@ scene.add(planet);
 const ringTexts = [
   'Galaxy of love From ...',//untuk deskripsi planet layer 1
   "I love you",//untuk deskripsi planet layer 2
-  "♡for ayaaa♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  "♡Aurellia Agustine♡",//untuk deskripsi planet layer 3
+  "02/08/2009",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
